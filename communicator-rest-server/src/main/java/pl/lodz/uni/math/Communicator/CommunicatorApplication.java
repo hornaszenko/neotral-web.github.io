28e10b98813ecb4a5a0633e9fcf4d82d40ca2910
Piotr Krzyminski <piotr.krzyminski96@gmail.com>
2018-12-24 10:24:14 +0100

    Initial commit

---
+++ communicator-rest-server/src/main/java/pl/lodz/uni/math/Communicator/CommunicatorApplication.java
@@ -0,0 +1,19 @@
+package pl.lodz.uni.math.Communicator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application runner.
+ *
+ * @author Piotr Krzyminski
+ */
+@SpringBootApplication
+public class CommunicatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunicatorApplication.class, args);
+	}
+
+}
+
---
+++ communicator-rest-server/src/main/java/pl/lodz/uni/math/Communicator/ServletInitializer.java
@@ -0,0 +1,18 @@
+package pl.lodz.uni.math.Communicator;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * Initializer for DispatcherServlet.
+ *
+ * @author Piotr Krzyminski
+ */
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CommunicatorApplication.class);
+	}
+}
+
